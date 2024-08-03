d056b172bf961cc27c45b737fb7bafd927f75d8b
Putra Pratama Nasution <putrapratamanst@gmail.com>
2024-08-03 10:57:26 +0700

    api retrieving blog posts

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\PostService;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    protected $postService;
+
+    public function __construct(PostService $postService)
+    {
+        $this->postService = $postService;
+    }
+
+    public function index(Request $request)
+    {
+        $posts = $this->postService->getAllPublishedPosts(10); // Ambil 10 post per halaman
+        return response()->json($posts);
+    }
+}
